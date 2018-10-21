67b36ef689a1898bea36148a42ba8c9da77d1986
dev-ritik <ritikkne@gmail.com>
2018-10-21 15:01:47 +0530

    features reduced

--- app/src/main/java/com/example/karthik/colorduneswallpaper/LiveTimeWallpaperService.java
+++ app/src/main/java/com/example/karthik/colorduneswallpaper/LiveTimeWallpaperService.java
@@ -1,19 +1,14 @@
 package com.example.karthik.colorduneswallpaper;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.Typeface;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.provider.AlarmClock;
 import android.service.wallpaper.WallpaperService;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 import java.text.SimpleDateFormat;
@@ -23,45 +18,41 @@ import java.util.Calendar;
  * Created by karthik on 6/6/18.
  */
 
-public class LiveTimeWallpaperService extends WallpaperService{
+public class LiveTimeWallpaperService extends WallpaperService {
 
-    static boolean numberSign = true,format24 = false,touchEnabled =false;
+    static boolean numberSign = true, format24 = false, touchEnabled = false;
     static Typeface[] typefaceText = new Typeface[5];
-    static int fontIndex = 0,overlayIndex = 0,dividerIndex = 0;
-    int xPos = 0,yPos = 0 ;
-    static Bitmap overlayBitmap[] = new Bitmap[5];
+    static int fontIndex = 0, overlayIndex = 0, dividerIndex = 0;
+    int xPos = 0, yPos = 0;
     Paint alphaPaint;
     SharedPreferences preferences;
     String time = "";
 
     @Override
-    public Engine onCreateEngine(){
+    public Engine onCreateEngine() {
         return new WallpaperEngine();
     }
 
-    private class WallpaperEngine extends  Engine{
-
-        Calendar calendar;
-        SimpleDateFormat timeFormat;
+    private class WallpaperEngine extends Engine {
 
         private final Handler handler = new Handler();
-
-        private final Runnable drawRunner  = new Runnable() {
+        Calendar calendar;
+        SimpleDateFormat timeFormat;
+        private Paint paint = new Paint();
+        private int width;
+        private int height;
+        private boolean visible = true;
+        private final Runnable drawRunner = new Runnable() {
             @Override
             public void run() {
                 draw();
             }
         };
 
-        private Paint paint = new Paint();
-        private  int width;
-        private int height;
-        private boolean visible = true;
 
-
-
-        public WallpaperEngine(){
+        private WallpaperEngine() {
             preferences = PreferenceManager.getDefaultSharedPreferences(LiveTimeWallpaperService.this);
+            Log.i("point 62", "WallpaperEngine");
 
             paint.setAntiAlias(true);
             paint.setColor(Color.WHITE);
@@ -69,67 +60,42 @@ public class LiveTimeWallpaperService extends WallpaperService{
             paint.setTextSize(100);
             paint.setDither(true);
 
-            typefaceText[0] = Typeface.createFromAsset(getAssets(),"Cinzel-Regular.ttf");
-            typefaceText[1] = Typeface.createFromAsset(getAssets(),"GloriaHallelujah.ttf");
-            typefaceText[2] = Typeface.createFromAsset(getAssets(),"Nexa-Light.otf");
-            typefaceText[3] = Typeface.createFromAsset(getAssets(),"Raleway-Light.ttf");
-            typefaceText[4] = Typeface.createFromAsset(getAssets(),"Roboto-Light.ttf");
+            typefaceText[0] = Typeface.createFromAsset(getAssets(), "Cinzel-Regular.ttf");
+            typefaceText[1] = Typeface.createFromAsset(getAssets(), "GloriaHallelujah.ttf");
+            typefaceText[2] = Typeface.createFromAsset(getAssets(), "Nexa-Light.otf");
+            typefaceText[3] = Typeface.createFromAsset(getAssets(), "Raleway-Light.ttf");
+            typefaceText[4] = Typeface.createFromAsset(getAssets(), "Roboto-Light.ttf");
 
             alphaPaint = new Paint();
             alphaPaint.setAlpha(80);
-
-            overlayBitmap[0] = BitmapFactory.decodeResource(getResources(),R.mipmap.dots);
-            overlayBitmap[1] = BitmapFactory.decodeResource(getResources(),R.mipmap.honeycomb);
-            overlayBitmap[2] = BitmapFactory.decodeResource(getResources(),R.mipmap.square);
-            overlayBitmap[3] = BitmapFactory.decodeResource(getResources(),R.mipmap.triangle);
-            overlayBitmap[4] = BitmapFactory.decodeResource(getResources(),R.mipmap.teapot);
 
             handler.post(drawRunner);
         }
 
 
         @Override
-        public void onVisibilityChanged(boolean visible){
+        public void onVisibilityChanged(boolean visible) {
             this.visible = visible;
-            if(visible){
+            if (visible) {
                 handler.post(drawRunner);
-            }else {
+            } else {
                 handler.removeCallbacks(drawRunner);
             }
         }
 
         @Override
-        public  void onSurfaceDestroyed(SurfaceHolder holder){
+        public void onSurfaceDestroyed(SurfaceHolder holder) {
             super.onSurfaceDestroyed(holder);
+            Log.i("point 105", "destroyed");
         }
 
         @Override
-        public void onSurfaceChanged(SurfaceHolder holder,int format,int width,int height){
+        public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             this.width = width;
             this.height = height;
-            super.onSurfaceChanged(holder,format,width,height);
+            super.onSurfaceChanged(holder, format, width, height);
+            Log.i("point 113", "changed");
         }
-
-        @Override
-        public void onTouchEvent(MotionEvent event){
-            if(event.getAction() == MotionEvent.ACTION_DOWN) {
-                if (touchEnabled) {
-                    float x = event.getX();
-                    float y = event.getY();
-
-                            Rect rect = new Rect();
-                            paint.getTextBounds(time, 0, time.length(), rect);
-                            if (rect.contains((int)(x-xPos),(int)(y-yPos))) {
-                                Intent openClockIntent = new Intent(AlarmClock.ACTION_SET_ALARM);
-                                openClockIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(openClockIntent);
-                            }
-                        }
-
-                    }
-                    super.onTouchEvent(event);
-                }
-
 
 
         private void draw() {
@@ -148,34 +114,10 @@ public class LiveTimeWallpaperService extends WallpaperService{
 
             if (format24) {
                 color = Color.parseColor("#" + ((new SimpleDateFormat("HHmmss")).format(calendar.getTime())));
-                if (dividerIndex == 0) {
-                    timeFormat = new SimpleDateFormat("HHmmss");
-                } else if (dividerIndex == 1) {
-                    timeFormat = new SimpleDateFormat("HH.mm.ss");
-                } else if (dividerIndex == 2) {
-                    timeFormat = new SimpleDateFormat("HH:mm:ss");
-                } else if (dividerIndex == 3) {
-                    timeFormat = new SimpleDateFormat("HH mm ss");
-                } else if (dividerIndex == 4) {
-                    timeFormat = new SimpleDateFormat("HH|mm|ss");
-                } else if (dividerIndex == 5) {
-                    timeFormat = new SimpleDateFormat("HH/mm/ss");
-                }
+                timeFormat = new SimpleDateFormat("HH.mm.ss");
             } else {
                 color = Color.parseColor("#" + ((new SimpleDateFormat("hhmmss")).format(calendar.getTime())));
-                if (dividerIndex == 0) {
-                    timeFormat = new SimpleDateFormat("hhmmss");
-                } else if (dividerIndex == 1) {
-                    timeFormat = new SimpleDateFormat("hh.mm.ss");
-                } else if (dividerIndex == 2) {
-                    timeFormat = new SimpleDateFormat("hh:mm:ss");
-                } else if (dividerIndex == 3) {
-                    timeFormat = new SimpleDateFormat("hh mm ss");
-                } else if (dividerIndex == 4) {
-                    timeFormat = new SimpleDateFormat("hh|mm|ss");
-                } else if (dividerIndex == 5) {
-                    timeFormat = new SimpleDateFormat("hh/mm/ss");
-                }
+                timeFormat = new SimpleDateFormat("hh.mm.ss");
             }
 
             time = "#" + timeFormat.format(calendar.getTime());
@@ -187,12 +129,9 @@ public class LiveTimeWallpaperService extends WallpaperService{
             try {
                 canvas = holder.lockCanvas();
                 if (canvas != null) {
+                    Log.i("point 193", "canvas" + canvas);
 
                     canvas.drawColor(color);
-
-                    if (overlayIndex > 0) {
-                        canvas.drawBitmap(overlayBitmap[overlayIndex - 1], 0, 0, alphaPaint);
-                    }
 
                     if (numberSign) {
                         xPos = (canvas.getWidth() / 2) - (int) (paint.measureText(time) / 2);
@@ -218,9 +157,9 @@ public class LiveTimeWallpaperService extends WallpaperService{
             }
         }
 
-        }
-
     }
+
+}
 
 
 
